beeb1a911fd9c2d427bfece74fcecbab84f2c5c7
fidelia <fidelialtm13@gmail.com>
2023-06-05 23:09:48 +0200

    react3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
 import "./style.css"
 
 export default function App (){
-  return "hi "
+  return(
+    <>
+    <form className="new-item-form">
+ 
+    <div className="form-row">
+    <label htmlFor="item">New item</label>
+    <input type="text" name="item" />
+    </div>
+    <button className="btn">ADD</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+      <ul className="List">
+           <li>
+             <label>
+             <input type="checkbox"/>
+             item 1
+             </label>
+             <button className="btn btn-danger">
+             DELETE
+             </button>
+           </li>
+           <li>
+             <label>
+             <input type="checkbox"/>
+             item 2S
+             </label>
+             <button className="btn btn-danger">
+             DELETE
+             </button>
+           </li>
+       </ul>
+    </>
+  
+  ) 
 }
